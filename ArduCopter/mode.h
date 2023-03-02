d4c4d2218ad6e66640725624c9e98db61791ed55
Atharvhardikar43 <107064950+Atharvhardikar43@users.noreply.github.com>
2023-03-02 09:30:32 +0530

    Update mode.h

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -39,7 +39,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
-
+        FTS =          43,  // Land safely to a nearby position if copter goes out of bounds
         // Mode number 127 reserved for the "drone show mode" in the Skybrush
         // fork at https://github.com/skybrush-io/ardupilot
     };
